dd555097c259d0a61cb0783fe507c2e915775167
notlegos <roger@mustacich.com>
2024-06-22 18:25:47 -0700

    Update pxt.json, main.blocks, main.ts, background.ts, irreceiver.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+makerbit.onIrButton(IrButton.Any, IrButtonAction.Pressed, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(500)
+    basic.clearScreen()
+})
+makerbit.connectIrReceiver(DigitalPin.P1, IrProtocol.NEC)
+basic.showIcon(IconNames.Surprised)
 basic.forever(function () {
 	
 })
